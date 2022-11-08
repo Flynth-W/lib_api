0ba0f73ca310c358df64e70bcf2c5871bcaef70b
flynth <flynth.dev@gmail.com>
2022-11-07 22:55:01 -0400

    Export modules

---
+++ mods.ts
@@ -0,0 +1,4 @@
+export { Model } from "./model/Model.ts"
+export { Connect }  from "./data/Connect.ts"
+export type { property } from "./model/types/property.ts"
+export type { response } from "./model/types/response.ts"
