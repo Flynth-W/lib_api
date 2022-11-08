5a6ff4f0e48169570ede3330c53c8a5329e12626
flynth <flynth.dev@gmail.com>
2022-11-07 22:53:40 -0400

    Connect Mongodb Ready

---
+++ data/CRUD.ts
@@ -0,0 +1,39 @@
+type Filter=string | Record<never,never>
+
+
+export class CRUD {
+    clltn:any
+    constructor(collection:any){
+        this.clltn=collection
+    }
+    create(obj:Record<never,never>[]){
+        return this.clltn.insertMany(obj)
+    }
+    read(filter?:Filter){
+        if( typeof(filter) == "string" ){
+            return this.clltn.find({[filter]:{$ne:null}}).toArray()
+        }
+        return this.clltn.find(filter).toArray()
+    }
+    update(filter:Filter,obj:Record<never,never>){
+        if( typeof(filter) == "string" ){
+            return this.clltn.updateMany({[filter]:{$ne:null}},{$set :{...obj}})
+        }
+        return this.clltn.updateMany(filter,{$set :{...obj}})
+    }
+    delete(filter:Filter){
+        if( typeof(filter) == "string" ){
+            return this.clltn.deleteMany({[filter]:{$ne:null}})
+        }
+        return this.clltn.deleteMany(filter)
+    }
+    deleteProp(filter:Filter,prop:string){
+        if( typeof(filter) == "string" ){
+            return this.clltn.updateMany({[filter]:{$ne:null}},{$unset :{[prop]:0}})
+        }
+        return this.clltn.updateMany(filter,{$unset :{[prop]:0}})
+    }
+}
+
+
+
---
+++ data/Connect.ts
@@ -0,0 +1,27 @@
+import {  MongoClient} from "../deps.ts";
+import { CRUD } from "./CRUD.ts"
+
+
+
+
+export async function  Connect(urlMongo:string,dbs:string,clltn:string){
+    const client = new MongoClient();
+    await client.connect(urlMongo);
+    const db = client.database(dbs);
+    const _clltn = db.collection(clltn)
+    const cltm= new CRUD(_clltn)
+    return cltm
+}
+
+//Forma de usar :
+
+/*  const data={
+ *      urlMongo:"mongodb://127.0.0.1:8011",
+ *      dbs:"exampleOne",
+ *      clltn:"collectionExample"
+ *  }
+ *  
+ *  const userClltn=await connect(data.urlMongo,data.dbs,data.clltn)
+ *  
+ *  console.log(await userClltn.read() )
+ */
