f0dd238b69729bfc9bca7c76340be92b6373f66d
flynth <flynth.dev@gmail.com>
2022-11-29 10:41:15 -0400

    Include try().cath() Connect

--- data/Connect.ts
+++ data/Connect.ts
@@ -5,11 +5,18 @@ import {  MongoClient} from "../deps.ts";
 
 
 export async function  Connect(urlMongo:string,dbs:string,clltn:string){
+  try{
     const client = new MongoClient();
     await client.connect(urlMongo);
     const db = client.database(dbs);
     const _clltn = db.collection(clltn)
     return _clltn
+  }catch(_e){
+    console.log("no listent" )
+    console.log(`:: ${urlMongo} | ${dbs} | ${clltn}`)
+    console.error(_e)
+    Deno.exit()
+  }
 //    const cltm= new CRUD(_clltn)
 //    return cltm
 }
