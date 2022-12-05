0f757832b7ab30d4eca78c3fde0fa42f096250dd
flynth <flynth.dev@gmail.com>
2022-12-05 11:39:22 -0400

    Fix: route type response

--- model/interface/Resp.ts
+++ model/interface/Resp.ts
@@ -1,16 +1,16 @@
-import { respons } from "../types/response.ts"
+import { response } from "../types/response.ts"
 
 export class Resp{
-    static PropNot(prop:string):respons{
+    static PropNot(prop:string):response{
         return {body: {[prop]:null} ,statusText:"no exsite esa propiedad en el modelo",ok:false}
     }
-    static PropVoid(prop:string):respons{
+    static PropVoid(prop:string):response{
         return {body: {[prop]:null} ,statusText:"la propiedad esta vacia",ok:false}
     }
-    static CheckFail(prop:string):respons{
+    static CheckFail(prop:string):response{
         return {body: {[prop]:null} ,statusText:"property fail check",ok:false}
     }
-    static CheckOk(obj:{}):respons{
+    static CheckOk(obj:{}):response{
         return {body: obj ,statusText:"OK",ok:true}
     }
 }
